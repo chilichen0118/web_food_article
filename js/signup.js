5fd7623ddf01712e2b3cb1afcb89b4f9a49b3e68
chilichen0118 <chilichen0118@gmail.com>
2022-06-13 09:25:26 +0800

    登入/註冊頁面完成

---
+++ js/signup.js
@@ -0,0 +1,34 @@
+// var index = 0;
+// var signupBtn = document.getElementsByClassName("status-gourmet");
+
+// function showBtn() {
+//     for (let s = 0; s < signupBtn.length; s++) {
+//         signupBtn[s].classList.remove("s-g-style");
+//     }
+//     signupBtn[index].classList.add("s-g-style");
+// }
+
+// function clickBtn(s) {
+//     index = s ;
+//     showBtn();
+// }
+
+// for (let s = 0; s < signupBtn.length; s++) {
+//     signupBtn[s].onclick = function() {
+//         clickBtn(s);
+//     };
+    
+// }
+
+// $('.status-gourmet').click( function(){
+//     if ( $(this).hasClass('s-g-style') ) {
+//         $(this).removeClass('s-g-style');
+//     } else {
+//         $('.status-gourmet.s-g-style').removeClass('s-g-style');
+//         $('.status-gourmet').addClass('s-g-style');    
+//     }
+// });
+
+
+
+
